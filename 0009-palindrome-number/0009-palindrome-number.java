14324cd648dc1b7dc2ea4e721ae5bc581fd43e14
Aakash Yadav <aakashydv0192@gmail.com>
2026-08-30 12:26:35 +0530

    Time: 4 ms (100%), Space: 45.8 MB (87.01%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0 || (x>0 && x%10==0 )){
+            return false;
+        }
+        int org = x;
+        int y = 0;
+        while( x != 0){
+        y=y*10+x%10;
+        x=x/10;
+        }
+
+         return org == y;
+    }
+}
